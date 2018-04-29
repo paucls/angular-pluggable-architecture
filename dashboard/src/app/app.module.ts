2a630002468807e8f251f031e706814f06dd85ae
Paulo Clavijo Esteban <paucls@gmail.com>
2018-04-29 10:21:27 +0100

    New dashboard module and component

--- dashboard/src/app/app.module.ts
+++ dashboard/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DashboardModule } from './dashboard/dashboard.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DashboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ dashboard/src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [DashboardComponent],
+  exports: [DashboardComponent]
+})
+export class DashboardModule { }
